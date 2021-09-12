c7513bc10431a6300e15c3fa4aad04c8b65c2eb3
Josué Teodoro Moreira <teodoro.josue@protonmail.ch>
2021-09-12 11:55:28 -0300

    Added gif wallpapers to be played with paperview and qutebrowser config

---
+++ qutebrowser/config.py
@@ -0,0 +1,116 @@
+# Home page
+c.url.start_pages = ["https://www.8bitdash.com/"]
+
+# Dvorak vim bindings
+config.bind("h", "fake-key <Left>", "normal")
+config.bind("t", "fake-key <Down>", "normal")
+config.bind("n", "fake-key <Up>", "normal")
+config.bind("s", "fake-key <Right>", "normal")
+
+# Nord theme 
+bg_default          = "#2e3440"
+bg_lighter          = "#3b4252"
+bg_selection        = "#4c566a"
+fg_disabled         = "#565c64"
+fg_default          = "#d8dee9"
+bg_lightest         = "#e5e9f0"
+fg_error            = "#bf616a"
+bg_hint             = "#ebcb8b"
+fg_matched_text     = "#a3be8c"
+bg_passthrough_mode = "#88c0d0"
+bg_insert_mode      = "#81a1c1"
+bg_warning          = "#b48ead"
+
+c.colors.completion.fg = fg_default
+c.colors.completion.odd.bg = bg_lighter
+c.colors.completion.even.bg = bg_default
+c.colors.completion.category.fg = bg_hint
+c.colors.completion.category.bg = bg_default
+c.colors.completion.category.border.top = bg_default
+c.colors.completion.category.border.bottom = bg_default
+c.colors.completion.item.selected.fg = fg_default
+c.colors.completion.item.selected.bg = bg_selection
+c.colors.completion.item.selected.border.top = bg_selection
+c.colors.completion.item.selected.border.bottom = bg_selection
+c.colors.completion.item.selected.match.fg = fg_matched_text
+c.colors.completion.match.fg = fg_matched_text
+c.colors.completion.scrollbar.fg = fg_default
+c.colors.completion.scrollbar.bg = bg_default
+c.colors.contextmenu.disabled.bg = bg_lighter
+c.colors.contextmenu.disabled.fg = fg_disabled
+c.colors.contextmenu.menu.bg = bg_default
+c.colors.contextmenu.menu.fg =  fg_default
+c.colors.contextmenu.selected.bg = bg_selection
+c.colors.contextmenu.selected.fg = fg_default
+c.colors.downloads.bar.bg = bg_default
+c.colors.downloads.start.fg = bg_default
+c.colors.downloads.start.bg = bg_insert_mode
+c.colors.downloads.stop.fg = bg_default
+c.colors.downloads.stop.bg = bg_passthrough_mode
+c.colors.downloads.error.fg = fg_error
+c.colors.hints.fg = bg_default
+c.colors.hints.bg = bg_hint
+c.colors.hints.match.fg = fg_default
+c.colors.keyhint.fg = fg_default
+c.colors.keyhint.suffix.fg = fg_default
+c.colors.keyhint.bg = bg_default
+c.colors.messages.error.fg = bg_default
+c.colors.messages.error.bg = fg_error
+c.colors.messages.error.border = fg_error
+c.colors.messages.warning.fg = bg_default
+c.colors.messages.warning.bg = bg_warning
+c.colors.messages.warning.border = bg_warning
+c.colors.messages.info.fg = fg_default
+c.colors.messages.info.bg = bg_default
+c.colors.messages.info.border = bg_default
+c.colors.prompts.fg = fg_default
+c.colors.prompts.border = bg_default
+c.colors.prompts.bg = bg_default
+c.colors.prompts.selected.bg = bg_selection
+c.colors.prompts.selected.fg = fg_default
+c.colors.statusbar.normal.fg = fg_matched_text
+c.colors.statusbar.normal.bg = bg_default
+c.colors.statusbar.insert.fg = bg_default
+c.colors.statusbar.insert.bg = bg_insert_mode
+c.colors.statusbar.passthrough.fg = bg_default
+c.colors.statusbar.passthrough.bg = bg_passthrough_mode
+c.colors.statusbar.private.fg = bg_default
+c.colors.statusbar.private.bg = bg_lighter
+c.colors.statusbar.command.fg = fg_default
+c.colors.statusbar.command.bg = bg_default
+c.colors.statusbar.command.private.fg = fg_default
+c.colors.statusbar.command.private.bg = bg_default
+c.colors.statusbar.caret.fg = bg_default
+c.colors.statusbar.caret.bg = bg_warning
+c.colors.statusbar.caret.selection.fg = bg_default
+c.colors.statusbar.caret.selection.bg = bg_insert_mode
+c.colors.statusbar.progress.bg = bg_insert_mode
+c.colors.statusbar.url.fg = fg_default
+c.colors.statusbar.url.error.fg = fg_error
+c.colors.statusbar.url.hover.fg = fg_default
+c.colors.statusbar.url.success.http.fg = bg_passthrough_mode
+c.colors.statusbar.url.success.https.fg = fg_matched_text
+c.colors.statusbar.url.warn.fg = bg_warning
+c.colors.tabs.bar.bg = bg_default
+c.colors.tabs.indicator.start = bg_insert_mode
+c.colors.tabs.indicator.stop = bg_passthrough_mode
+c.colors.tabs.indicator.error = fg_error
+c.colors.tabs.odd.fg = fg_default
+c.colors.tabs.odd.bg = bg_lighter
+c.colors.tabs.even.fg = fg_default
+c.colors.tabs.even.bg = bg_default
+c.colors.tabs.pinned.even.bg = bg_passthrough_mode
+c.colors.tabs.pinned.even.fg = bg_lightest
+c.colors.tabs.pinned.odd.bg = fg_matched_text
+c.colors.tabs.pinned.odd.fg = bg_lightest
+c.colors.tabs.pinned.selected.even.bg = bg_selection
+c.colors.tabs.pinned.selected.even.fg = fg_default
+c.colors.tabs.pinned.selected.odd.bg = bg_selection
+c.colors.tabs.pinned.selected.odd.fg = fg_default
+c.colors.tabs.selected.odd.fg = fg_default
+c.colors.tabs.selected.odd.bg = bg_selection
+c.colors.tabs.selected.even.fg = fg_default
+c.colors.tabs.selected.even.bg = bg_selection
+c.colors.webpage.bg = bg_default
+
+config.load_autoconfig(True)
